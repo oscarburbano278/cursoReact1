dfe8202e20c6ab749b0e50b1a07843d307fe772d
oscar burbano <oscar10181@hotmail.com>
2024-01-17 17:45:55 -0500

    se cambiaron los enunciados con respecto a los testimonios y se implemento jsx con los props para no duplicar el codigo y reutilizarlos con los demas testimonios

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
-      <Testimonio />
+      <div className='contenedor-principal'>
+        <h1>Testimonios de nuestros Estudiantes</h1>
+        <Testimonio
+        imagen = 'camila'
+        nombre ='Camila Lopez'
+        cargo = 'gerente de proyectos'
+        pais = 'Costa rica'
+        testimonio = 'Hemos encontrado en ZWS la ayuda que necesitábamos para implementar nuestra página web. Total apertura con ellos en las ideas y requerimientos adecuados a nuestro enfoque del proyecto.'
+         />
+      </div>      
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,13 +1,15 @@
 import React from "react";
+import '../estilos/Testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
   return(
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require("../imagenes/medellin1.jpg")} alt="foto 1 de medellin"/>
+      <img className="imagen-testimonio"
+      src={require(`../imagenes/${props.imagen}.png`)} alt="foto camila lopez"/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"> Historia de Medellin </p>
-        <br/>
-        <p className="cargo-testimonio"> Medellín se ha convertido en un referente en Colombia y el mundo. Su actividad comercial, industrial, cultural, religiosa, social y deportiva han traído desarrollo y un estilo de vida amable y prometedor. Su historia se remonta al siglo XVII, cuando comenzaría el proyecto que poco a poco se iría convirtiendo en el lugar que es hoy. </p>
+        <p className="nombre-testimonio">{props.nombre}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.pais}</p>
+        <p className="texto-testimonio"> {props.testimonio} </p>
       </div>
     </div>
   ); 
