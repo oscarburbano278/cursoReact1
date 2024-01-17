381be96ac082f5c1e5f75493154f872da6984706
oscar burbano <oscar10181@hotmail.com>
2024-01-17 11:14:08 -0500

    modificando el contenido del blog, historia

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -5,8 +5,9 @@ function Testimonio(){
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio" src={require("../imagenes/medellin1.jpg")} alt="foto 1 de medellin"/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio"> primera imagen de Medellin </p>
-        <p className="cargo-testimonio"> estoy viviendo en Bogota pero soy de Medellin </p>
+        <p className="nombre-testimonio"> Historia de Medellin </p>
+        <br/>
+        <p className="cargo-testimonio"> Medellín se ha convertido en un referente en Colombia y el mundo. Su actividad comercial, industrial, cultural, religiosa, social y deportiva han traído desarrollo y un estilo de vida amable y prometedor. Su historia se remonta al siglo XVII, cuando comenzaría el proyecto que poco a poco se iría convirtiendo en el lugar que es hoy. </p>
       </div>
     </div>
   ); 
